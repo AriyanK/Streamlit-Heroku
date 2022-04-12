aa90d0753720e4d4247356770a20f3f25b300eb3
AriyanK <akumaraswamy@smu.edu>
2022-04-11 21:25:30 -0500

    Second commit

--- dataApp.py
+++ dataApp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
 st.title('Counter Example')
 if 'count' not in st.session_state:
     st.session_state.count = 0
