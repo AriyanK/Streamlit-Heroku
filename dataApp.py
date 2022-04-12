9fef5cee60a5afab9ac5fd64046103f715e54e24
AriyanK <akumaraswamy@smu.edu>
2022-04-11 21:04:42 -0500

    First commit

---
+++ dataApp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit as st
+st.title('Counter Example')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+st.write('Count = ', st.session_state.count)
+path = "C:/Users/skay9/Documents/Ary's_Stuff/MachineLearning/"
+bikeData = pd.read_csv(path + 'housing.csv')
+st.write(bikeData.describe())
